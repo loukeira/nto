fa611ef53a57cb045ccfc8ea4a7e1743bf3983d2
loukeira <markimsaraiva@gmail.com>
2018-09-30 13:06:34 -0300

    ok

--- SERV_BASE_NTO/data/creaturescripts/scripts/deathhp.lua
+++ SERV_BASE_NTO/data/creaturescripts/scripts/deathhp.lua
@@ -12,45 +12,48 @@ while skill < 7 do
 
 
 
-		local value,z,value2= max(cid,skill)
+		local value,hits,value2,hits2= max(cid,skill)
 
 		-- sword2, club1 e distance4, fist0, shield5, fishing 6
 		if (skill == 2 or skill == 0 or skill == 5 or skill == 6) and (getPlayerVocation(cid) == (1 or 2 or 12 )) then
 		value = value2
+		hits = hits2
 		elseif (skill == 1 or skill == 0 or skill == 5 or skill == 6) and (getPlayerVocation(cid) == (3 or 5 or 6 or 7 or 11 or 13 or 14 or 15) ) then
 		value = value2
+		hits = hits2
 		elseif (skill == 4 or skill == 0 or skill == 5 or skill == 6) and (getPlayerVocation(cid) == (4 or 8 or 9 or 10 ) ) then
 		value = value2
+		hits = hits2
 		end
 
 		if (getPlayerSkillTries(cid,skill) > 0.75*value) then
 
 		local matematica = getPlayerSkillTries(cid,skill) / 25
-		local matematica2 = matematica - (0.5*z)
+		local matematica2 = matematica - (0.5*hits)
 		doPlayerAddSkillTry(cid, skill, -matematica2)
 
 
 		elseif (getPlayerSkillTries(cid, skill) <= 0.75*value) and (getPlayerSkillTries(cid,skill) > 0.5*value) then
 		local matematica = getPlayerSkillTries(cid,skill) / 25
-		local matematica2 = matematica - (0.25*z)
+		local matematica2 = matematica - (0.25*hits)
 		doPlayerAddSkillTry(cid, skill, -matematica2)
 	
 
 		elseif (getPlayerSkillTries(cid, skill) <= 0.5*value) and (getPlayerSkillTries(cid,skill) > 0.25*value) then
 		local matematica = getPlayerSkillTries(cid,skill) / 25
-		local matematica2 = matematica - (0*z)
+		local matematica2 = matematica - (0*hits)
 		doPlayerAddSkillTry(cid, skill, -matematica2)
 		
 
-		elseif (getPlayerSkillTries(cid, skill) <= 0.25*value) and (getPlayerSkillTries(cid,skill) > 0*value) then
+		elseif (getPlayerSkillTries(cid, skill) <= 0.25*value) and (getPlayerSkillTries(cid,skill) >= 0*value) then
 
 		setPlayerSkillLevel(cid,skill,getPlayerSkillLevel(cid,skill)-1)
-		local newvalue,z2 = max(cid,skill)
+		local newvalue,hits2 = max(cid,skill)
 
 
 		--local matematica = getPlayerSkillTries(cid,skill) / 25
-		--local matematica2 = matematica - (0*z2)
-		local matematica3 = 0.75*z2
+		--local matematica2 = matematica - (0*hits2)
+		local matematica3 = 0.75*hits2
 		doPlayerAddSkillTry(cid, skill, matematica3)
 		end
 
@@ -71,8 +74,9 @@ end
 function max(cid ,skill)
  x = getPlayerSkillLevel(cid, skill) - 10
  k = (2*x) + 1
- z = math.pow(2, k)
- valor = z * 25
- valor2 = valor/(math.pow(2,x))
-return valor,z,valor2
+ hits = math.pow(2, k)
+ valornormal = hits * 25
+ valornormal2 = valor/(math.pow(2,x))
+ hits2 = valornormal2/25
+return valornormal,hits,valornormal2,hits2
 end
