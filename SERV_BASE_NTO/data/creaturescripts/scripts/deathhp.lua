a4239e68393b8bd91d4b839d05fb1d5947db9392
loukeira <markimsaraiva@gmail.com>
2018-09-30 01:06:57 -0300

    ok

--- SERV_BASE_NTO/data/creaturescripts/scripts/deathhp.lua
+++ SERV_BASE_NTO/data/creaturescripts/scripts/deathhp.lua
@@ -2,18 +2,38 @@ function onDeath(cid,attacker)
 	local HPFix = 158920
   if isPlayer(cid) then
 
+
+
+
 local skill = 0
 
 while skill < 7 do
-if getPlayerSkillLevel(cid,skill) > 10 then
+	if getPlayerSkillLevel(cid,skill) > 10 then
 
-getPlayerSkillTries(cid, skill)
 
-doPlayerSetSkillLevel(cid, skill, getPlayerSkillLevel(cid, skill)-1)
-end
+		local value = max(cid,skill)
+
+		if (getPlayerSkillTries(cid, skill) >= 0.75*value) then
+		doPlayerAddSkillTry(cid, skill, 0.5*value)
+
+		elseif (getPlayerSkillTries(cid, skill) >=0.5*value) and (getplayerskillTries(cid, skill) <0.75*value) then
+		doPlayerAddSkillTry(cid, skill, 0.25*value)
+
+		elseif (getplayerskillTries(cid, skill) >=0.25*value) and (getplayerskillTries(cid, skill) < 0.5*value) then
+		doPlayerAddSkillTry(cid, skill, 0)
+
+		elseif (getplayerskillTries(cid, skill) < 0.25*value) then
+		doPlayerSetSkillLevel(cid, skill, getPlayerSkillLevel(cid, skill)-1)
+		local newvalue = max(cid,skill)
+		doPlayerAddSkillTry(cid, skill, 0.75*newvalue)
+
+		end
+
+
+
+	end
 skill = skill + 1
 end
-
 
 
 
@@ -24,3 +44,10 @@ end
    return true
 end
 
+function max(cid ,skill)
+local x = getPlayerSkillLevel(cid, skill) - 10
+local k = (2*x) + 1
+local z = math.pow(2, k)
+local max = z * 25
+return max
+end
