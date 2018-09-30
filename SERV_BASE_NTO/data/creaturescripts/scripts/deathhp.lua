2d8b1acc36995bf2cbdd00f0d7f936e069f54fb1
loukeira <markimsaraiva@gmail.com>
2018-09-30 13:16:59 -0300

    ok

--- SERV_BASE_NTO/data/creaturescripts/scripts/deathhp.lua
+++ SERV_BASE_NTO/data/creaturescripts/scripts/deathhp.lua
@@ -50,11 +50,21 @@ while skill < 7 do
 		doPlayerSetSkillLevel(cid,skill,getPlayerSkillLevel(cid,skill)-1)
 		local newvalue,hits4 = max(cid,skill)
 
+		if (skill == 2 or skill == 0 or skill == 5 or skill == 6) and (getPlayerVocation(cid) == (1 or 2 or 12 )) then
+		value = value2
+		hits4 = hits2
+		elseif (skill == 1 or skill == 0 or skill == 5 or skill == 6) and (getPlayerVocation(cid) == (3 or 5 or 6 or 7 or 11 or 13 or 14 or 15) ) then
+		value = value2
+		hits4 = hits2
+		elseif (skill == 4 or skill == 0 or skill == 5 or skill == 6) and (getPlayerVocation(cid) == (4 or 8 or 9 or 10 ) ) then
+		value = value2
+		hits4 = hits2
+		end
 
 		--local matematica = getPlayerSkillTries(cid,skill) / 25
 		--local matematica2 = matematica - (0*hits2)
 		local matematica3 = 0.75*hits4
-		--doPlayerAddSkillTry(cid, skill, matematica3)
+		doPlayerAddSkillTry(cid, skill, matematica3)
 		end
 
 
