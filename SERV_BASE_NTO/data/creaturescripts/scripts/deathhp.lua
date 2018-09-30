65bfe4cf5ed132aff52abaebe72ff6428db1484f
loukeira <markimsaraiva@gmail.com>
2018-09-30 10:39:33 -0300

    ok

--- SERV_BASE_NTO/data/creaturescripts/scripts/deathhp.lua
+++ SERV_BASE_NTO/data/creaturescripts/scripts/deathhp.lua
@@ -15,26 +15,33 @@ while skill < 7 do
 		local value = max(cid,skill)
 
 
-		if (getPlayerSkillTries(cid, skill) >= 0.75*value) then
-		doPlayerSetSkillTries(cid, skill, 0.5*value,50)
-		doPlayerSendTextMessage(cid,18,"vc ficou com 50")
+		-- if (getPlayerSkillTries(cid, skill) >= 0.75*value) then
+		-- doPlayerSetSkillTries(cid, skill, 0.5*value,50)
+		-- doPlayerSendTextMessage(cid,18,"vc ficou com 50")
 
-		elseif (getPlayerSkillTries(cid, skill) >=0.5*value) and (getPlayerSkillTries(cid, skill) <0.75*value) then
-		doPlayerSetSkillTries(cid, skill, 0.25*value,25)
+		-- elseif (getPlayerSkillTries(cid, skill) >=0.5*value) and (getPlayerSkillTries(cid, skill) <0.75*value) then
+		-- doPlayerSetSkillTries(cid, skill, 0.25*value,25)
 
-		doPlayerSendTextMessage(cid,18,"vc ficou com 25")
+		-- doPlayerSendTextMessage(cid,18,"vc ficou com 25")
 
-		elseif (getPlayerSkillTries(cid, skill) >=0.25*value) and (getPlayerSkillTries(cid, skill) < 0.5*value) then
-		doPlayerSetSkillTries(cid, skill, 0,0)
-		doPlayerSendTextMessage(cid,18,"vc ficou com zero")
+		-- elseif (getPlayerSkillTries(cid, skill) >=0.25*value) and (getPlayerSkillTries(cid, skill) < 0.5*value) then
+		-- doPlayerSetSkillTries(cid, skill, 0,0)
+		-- doPlayerSendTextMessage(cid,18,"vc ficou com zero")
 
-		elseif (getPlayerSkillTries(cid, skill) < 0.25*value) then
-		doPlayerSendTextMessage(cid,18,"vc perdeu lvl")
-		doPlayerSendTextMessage(cid,18, "valor maximo: "..getPlayerSkillTries(cid,skill).." / "..max(cid,skill).." ")
+		-- elseif (getPlayerSkillTries(cid, skill) < 0.25*value) then
+		-- doPlayerSendTextMessage(cid,18,"vc perdeu lvl")
+		-- doPlayerSendTextMessage(cid,18, "valor maximo: "..getPlayerSkillTries(cid,skill).." / "..max(cid,skill).." ")
+		-- local newvalue = max(cid,skill)
+		-- doPlayerSetSkillTries(cid, skill, 0.75*newvalue)
+
+		-- end
+
+
+		--doPlayerSendTextMessage(cid,18,"vc perdeu lvl")
+		doPlayerSendTextMessage(cid,18, "antes: "..getPlayerSkillTries(cid,skill).." / "..max(cid,skill).." ")
 		local newvalue = max(cid,skill)
 		doPlayerSetSkillTries(cid, skill, 0.75*newvalue)
-
-		end
+		doPlayerSendTextMessage(cid,18, "depois: "..getPlayerSkillTries(cid,skill).." / "..max(cid,skill).." ")
 
 
 
