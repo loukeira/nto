f4314c8f9a67507bbf402fa04c34e1775073d259
loukeira <markimsaraiva@gmail.com>
2018-10-14 16:24:27 -0300

    ok

--- SERV_BASE_NTO/data/movements/scripts/mapmark_konoha.lua
+++ SERV_BASE_NTO/data/movements/scripts/mapmark_konoha.lua
@@ -6,15 +6,15 @@ function onStepIn(cid, item, position, lastPosition, fromPosition, toPosition, a
   local storage_mapmarks_konoha = 749732
 
   local id_acc = getPlayerAccountId(cid)
-  local sim = acc_stor(cid)
+  --local sim = acc_stor(cid)
 
   if (item.actionid == 14101) then
-    if (sim == 0) then
+    --if (sim == 0) then
             db.query("INSERT INTO `account_storage` (`account_id`, `key`, `value` ) VALUES ("..id_acc..", "..storage_mapmarks_konoha..",1);")
             mapmarks_konoha(cid, type, msg)
             doSendMagicEffect(getCreaturePosition(cid), 3)
 
-    end
+    --end
   end
  
 
