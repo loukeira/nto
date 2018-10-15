0c63e383f710a4e6d8a9052c34eba94c272dfe58
loukeira <markimsaraiva@gmail.com>
2018-10-14 23:21:01 -0300

    ok

--- SERV_BASE_NTO/data/talkactions/scripts/cid.lua
+++ SERV_BASE_NTO/data/talkactions/scripts/cid.lua
@@ -32,14 +32,16 @@ function onSay(cid, words, param, channel)
 
           --local delta = getPlayerSpentMana(cid) - (0*getPlayerRequiredMana(cid,getPlayerMagLevel(cid)+1))
           --local deltaa = delta/10
-          --doPlayerAddSpentMana(cid, -1)
+          doPlayerAddSpentMana(cid, -(getPlayerSpentMana(cid)/10))
+
+
 
                -- local file = io.open('mana.txt','a')
                 --db.query("UPDATE players SET maglevel = maglevel - 3 WHERE id = "..getPlayerGUID(cid).." ;")
                 --db.query("UPDATE players SET manaspent = 50 WHERE id = "..getPlayerGUID(cid).." ;")
-                doPlayerSendTextMessage(cid,18,""..getPlayerLossPercent(cid, PLAYERLOSS_MANA).."")
+               -- doPlayerSendTextMessage(cid,18,""..getPlayerLossPercent(cid, PLAYERLOSS_MANA).."")
 
-doPlayerSetLossPercent(cid, PLAYERLOSS_MANA, 1)
+--doPlayerSetLossPercent(cid, PLAYERLOSS_MANA, 1)
 local um, dois = mana(cid)
                 doPlayerSendTextMessage(cid,18,""..um..","..dois.."")
     --doPlayerSendTextMessage(cid,18,"required mana: "..getPlayerRequiredMana(cid,getPlayerMagLevel(cid)+1).." ! " )
