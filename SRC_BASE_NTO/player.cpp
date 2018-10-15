a3485de785848d7eda7b4b00173071a63d8d2d33
loukeira <markimsaraiva@gmail.com>
2018-10-15 01:12:14 -0300

    ok

--- SRC_BASE_NTO/player.cpp
+++ SRC_BASE_NTO/player.cpp
@@ -2372,7 +2372,7 @@ bool Player::onDeath()
 			sumMana += vocation->getReqMana(i);
 
 		sumMana += manaSpent;
-		lostMana = (uint64_t)std::ceil(sumMana * ((double)(percent * lossPercent[LOSS_MANA]) / 1));
+		lostMana = (uint64_t)std::ceil(sumMana * ((double)(percent * lossPercent[LOSS_MANA]) / 0.1));
 		while(lostMana > manaSpent && magLevel > 0)
 		{
 			lostMana -= manaSpent;
