22b0cfcf286e494419f0e251031f9e4ed090c8b0
msousay.94@gmail.com <mihawk123>
2018-10-15 20:34:48 -0300

    spells
    spells

--- SERV_BASE_NTO/data/spells/scripts/todos/strongheal.lua
+++ SERV_BASE_NTO/data/spells/scripts/todos/strongheal.lua
@@ -1,6 +1,6 @@
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_TYPE, COMBAT_HEALING)
-setCombatParam(combat, COMBAT_PARAM_EFFECT, 4)
+setCombatParam(combat, COMBAT_PARAM_EFFECT, 12)
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
 setCombatParam(combat, COMBAT_PARAM_DISPEL, CONDITION_PARALYZE)
 setHealingFormula(combat, COMBAT_FORMULA_LEVELMAGIC, 5, 5, 5, 6)
