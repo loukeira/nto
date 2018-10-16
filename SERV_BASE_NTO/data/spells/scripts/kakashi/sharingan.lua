a2a46cc0d7d65ad65737e816744d3aaf60d7fd21
loukeira <markimsaraiva@gmail.com>
2018-10-16 17:16:17 -0300

    ok

--- SERV_BASE_NTO/data/spells/scripts/kakashi/sharingan.lua
+++ SERV_BASE_NTO/data/spells/scripts/kakashi/sharingan.lua
@@ -1,13 +1,14 @@
 local tempo = 30 -- tempo em segundos.
-local effect = {176} -- effect no player, caso queira apenas 1, basta remover os outros numeros.
+local effect = {108} -- effect no player, caso queira apenas 1, basta remover os outros numeros.
+     
 local ml = 3 -- quantos ira aumentar o skill de ML
-local skillfist = 1 -- quantos ira aumentar o skill de Fist
-local skillsword = 1 -- quantos ira aumentar o skill de Sword
-local skillaxe = 1 -- quantos ira aumentar o skill de Axe
-local skillclub = 1 -- quantos ira aumentar o skill de Club
-local skilldistance = 1 -- quantos ira aumentar o skill de Distance
-local skillshield = 1 -- quantos ira aumentar o skill de Shield
-local health = 1 -- A cada 1 segundo quantos aumentar de vida
+local skillfist = 0 -- quantos ira aumentar o skill de Fist
+local skillsword = 0 -- quantos ira aumentar o skill de Sword
+local skillaxe = 0 -- quantos ira aumentar o skill de Axe
+local skillclub = 0 -- quantos ira aumentar o skill de Club
+local skilldistance = 0 -- quantos ira aumentar o skill de Distance
+local skillshield = 0 -- quantos ira aumentar o skill de Shield
+local health = 0 -- A cada 1 segundo quantos aumentar de vida
 local combat = createCombatObject()
 setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
      
@@ -37,9 +38,9 @@ setConditionParam(condition, CONDITION_PARAM_HEALTHGAIN, health)
 setConditionParam(condition, CONDITION_PARAM_HEALTHTICKS, 1000)
 setCombatCondition(combat, condition)
             
-function magicEffect557(tempo2,tempo3,cid)
+function magicEffect1553(tempo2,tempo3,cid)
 if (isCreature(cid)) then
-    if getPlayerStorageValue(cid, 102053) > 0 and getCreatureCondition(cid, CONDITION_REGENERATION, 1) then
+    if getPlayerStorageValue(cid, 212053) > 0 and getCreatureCondition(cid, CONDITION_REGENERATION, 1) then
         for i=1, #effect do
         local position = {x=getPlayerPosition(cid).x, y=getPlayerPosition(cid).y, z=getPlayerPosition(cid).z}
 doSendMagicEffect(position, effect[i])  
@@ -50,16 +51,16 @@ end
                   
 function onCastSpell(cid, var)
 local position127 = {x=getPlayerPosition(cid).x, y=getPlayerPosition(cid).y, z=getPlayerPosition(cid).z}
-if getPlayerStorageValue(cid, 102053) ~= 1 or getCreatureCondition(cid, CONDITION_REGENERATION, 1) == false then
+if getPlayerStorageValue(cid, 212053) ~= 1 or getCreatureCondition(cid, CONDITION_REGENERATION, 1) == false then
     doCombat(cid, combat, var)
     tempo2 = 0
     while (tempo2 ~= (tempo*1000)) do
-        addEvent(magicEffect557, tempo2, tempo2, tempo*1000, cid)
+        addEvent(magicEffect1553, tempo2, tempo2, tempo*1000, cid)
         tempo2 = tempo2 + 300
     end
     setPlayerStorageValue(cid, 102053,1) -- storage verifica transformado, quando = 1 player esta transformado.
-    doCreatureSay(cid, "Raiton Yoroi", TALKTYPE_MONSTER)
-    doSendMagicEffect(position127, 134)
+    doCreatureSay(cid, "Sharingan", TALKTYPE_MONSTER)
+    doSendMagicEffect(position127, 108)
 else
     doPlayerSendCancel(cid, "Sorry, you are transformed.")
 end
