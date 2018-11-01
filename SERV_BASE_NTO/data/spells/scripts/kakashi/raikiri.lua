21159735793df58a2e0942a47317ba6084180e2b
loukeira <markimsaraiva@gmail.com>
2018-11-01 19:14:08 -0300

    ok

--- SERV_BASE_NTO/data/spells/scripts/kakashi/raikiri.lua
+++ SERV_BASE_NTO/data/spells/scripts/kakashi/raikiri.lua
@@ -4,18 +4,20 @@ setCombatFormula(combat, COMBAT_FORMULA_LEVELMAGIC, -50.2, 1, -80.2, 1)
 
 function onCastSpell(cid, var)
 target = getCreatureTarget(cid)
-if target then
+if not target then doPlayerSendCancel(cid, "Voce tem que estar mirando em alguem!") return true end
+
 local waittime = 40
 local storage = 113007
 
 if exhaustion.check(cid, storage) then
 doCreatureSay(cid, "Aguarde " .. exhaustion.get(cid, storage) .. " segundos para usar a spell novamente.", TALKTYPE_MONSTER)
-return false
+return true
 end
 
 exhaustion.set(cid, storage, waittime)
 local position1 = {x=getThingPosition(getCreatureTarget(cid)).x, y=getThingPosition(getCreatureTarget(cid)).y, z=getThingPosition(getCreatureTarget(cid)).z}
 
 doSendMagicEffect(position1, 138)
+
 return doCombat(cid, combat, var)
 end
