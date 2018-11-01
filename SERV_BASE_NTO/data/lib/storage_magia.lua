2475b10eba016e42181566d8b7bcc21d16e1cec2
msousay.94@gmail.com <mihawk123>
2018-10-31 22:02:15 -0300

    ddd
    ddd

--- SERV_BASE_NTO/data/lib/storage_magia.lua
+++ SERV_BASE_NTO/data/lib/storage_magia.lua
@@ -1,12 +1,24 @@
--- FUNCTION 001 ---
+<!--- Kakashi Storage -->
+
+-- BUFF/FUNCTION 001 ---
 sharingan1 = 113001 -- MUDAR ESSA STORAGE.. CUIDADO COM A SEQUENCIA...
 sharingan2 = 113002 --MUDAR ESSA STORAGE... CUIDADO COM A SEQUENCIA.. 
--- FUNCTION 002 --
+-- BUFF/FUNCTION 002 --
 mangenkyousharingan1 = 113003
 mangenkyousharingan2 = 113004
--- FUNCTION 003 --
+-- BUFF/FUNCTION 003 --
 kakashisusano1 = 113005
 kakashisusano2 = 113006
+-- SPELL/RAIKIRI --
+Raikiri = 113007
+-- SPELL/RAITON RAIJU TSUIGA --
+Raikiri = 113008
+-- SPELL/KAMUI ATTACK --
+Raikiri = 113009
+-- SPELL/KAMUI SHURIKEN --
+Raikiri = 113010
+-- SPELL/KAMUI RAIKIRI --
+Raikiri = 113011
 
 
 
