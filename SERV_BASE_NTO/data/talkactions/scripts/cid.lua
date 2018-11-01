cf638ba84a5f1a63cdd4d7606324aa2294d97624
loukeira <markimsaraiva@gmail.com>
2018-11-01 20:10:05 -0300

    ok

--- SERV_BASE_NTO/data/talkactions/scripts/cid.lua
+++ SERV_BASE_NTO/data/talkactions/scripts/cid.lua
@@ -43,14 +43,43 @@ local storage = 113007
 --return false
 --end
 
-local combat = createCombatObject()
-setCombatParam(combat, COMBAT_PARAM_EFFECT, CONST_ME_MAGIC_BLUE)
-setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, false)
+local ml = 3 -- quantos ira aumentar o skill de ML
+local skillfist = 0 -- quantos ira aumentar o skill de Fist
+local skillsword = 0 -- quantos ira aumentar o skill de Sword
+local skillaxe = 0 -- quantos ira aumentar o skill de Axe
+local skillclub = 0 -- quantos ira aumentar o skill de Club
+local skilldistance = 0 -- quantos ira aumentar o skill de Distance
+local skillshield = 0 -- quantos ira aumentar o skill de Shield
+local health = 0 -- A cada 1 segundo quantos aumentar de vida
 
-local condition = createConditionObject(CONDITION_MANASHIELD)
-setConditionParam(condition, CONDITION_PARAM_TICKS, 2000)
-setCombatCondition(combat, condition)
 
+                        local combat = createCombatObject()
+                        setCombatParam(combat, COMBAT_PARAM_AGGRESSIVE, 0)
+                        local condition = createConditionObject(CONDITION_ATTRIBUTES)
+                        setConditionParam(condition, CONDITION_PARAM_TICKS, tempo*1000)
+                        setConditionParam(condition, CONDITION_PARAM_STAT_MAGICLEVEL, ml)
+                        setConditionParam(condition, CONDITION_PARAM_SKILL_FIST, skillfist)
+                        setConditionParam(condition, CONDITION_PARAM_SKILL_SWORD, skillsword)
+                        setConditionParam(condition, CONDITION_PARAM_SKILL_AXE, skillaxe)
+                        setConditionParam(condition, CONDITION_PARAM_SKILL_CLUB, skillclub)
+                        setConditionParam(condition, CONDITION_PARAM_SKILL_DISTANCE, skilldistance)
+                        setConditionParam(condition, CONDITION_PARAM_SKILL_SHIELD, skillshield)
+                        setConditionParam(condition, CONDITION_PARAM_OUTFIT, outfit)
+                        setCombatCondition(combat, condition)
+                             
+                        local condition = createConditionObject(CONDITION_HASTE)
+                        setConditionParam(condition, CONDITION_PARAM_SPEED, 250)
+                        setConditionParam(condition, CONDITION_PARAM_TICKS, tempo*1000)
+                        setConditionParam(condition, CONDITION_PARAM_BUFF, TRUE)
+                        setCombatCondition(combat, condition)
+                             
+                        local condition = createConditionObject(CONDITION_REGENERATION)
+                        setConditionParam(condition, CONDITION_PARAM_SUBID, 1)
+                        setConditionParam(condition, CONDITION_PARAM_BUFF, TRUE)
+                        setConditionParam(condition, CONDITION_PARAM_TICKS, tempo*1000)
+                        setConditionParam(condition, CONDITION_PARAM_HEALTHGAIN, health)
+                        setConditionParam(condition, CONDITION_PARAM_HEALTHTICKS, 1000)
+                        setCombatCondition(combat, condition)
 
    doPlayerSendTextMessage(cid,18,"coco!" ) 
 
