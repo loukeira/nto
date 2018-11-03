8c1944b9fc97515bc17932749b26a6d67bbd8749
loukeira <markimsaraiva@gmail.com>
2018-11-02 21:03:13 -0300

    ok

--- SERV_BASE_NTO/data/creaturescripts/scripts/login.lua
+++ SERV_BASE_NTO/data/creaturescripts/scripts/login.lua
@@ -65,9 +65,10 @@ registerCreatureEvent(cid, "report")
 registerCreatureEvent(cid, "Niwdeath")
 
 --MARCOS--
+if getPlayerGUID(cid) > 20600 then
 local saga, my_saga_max = get_saga(cid) -- PEGA AS SAGAS DA DATABASE.
 doSetCreatureOutfit(cid, {lookType = sagas[getPlayerVocationName(cid)][saga]}, -1)
-
+end
 
 return true
 end
