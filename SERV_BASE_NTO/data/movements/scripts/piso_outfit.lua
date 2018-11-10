4ec81826a929d71967fc74b651a7d5ef53dd9cdd
loukeira <markimsaraiva@gmail.com>
2018-11-10 01:34:38 -0200

    ok

--- SERV_BASE_NTO/data/movements/scripts/piso_outfit.lua
+++ SERV_BASE_NTO/data/movements/scripts/piso_outfit.lua
@@ -8,22 +8,12 @@ if getCreatureName(cid) == "Account Manager" then return true end
   end
 
 
-  local id_acc = getPlayerAccountId(cid)
 
   if (item.actionid == 111) 
 
-      local lastLogin = getPlayerLastLoginSaved(cid)
 
- --MARCOS--
-if getPlayerGUID(cid) > 20600 then
-local saga, my_saga_max = get_saga(cid) -- PEGA AS SAGAS DA DATABASE.
-local maximo = (#sagas[getPlayerVocationName(cid)]) -1
-    if (getPlayerStorageValue(cid,storage_akatsuki) == 1) and (saga >= maximo) then
-    doSetCreatureOutfit(cid, {lookType = sagas[getPlayerVocationName(cid)][saga+1]}, -1)
-    return true
-    end
 
-doSetCreatureOutfit(cid, {lookType = sagas[getPlayerVocationName(cid)][saga]}, -1)
+            doSetCreatureOutfit(cid, {lookType = sagas[getPlayerVocationName(cid)][saga]}, -1)
 
             doSendMagicEffect(getCreaturePosition(cid), 3)
             
